8bd4db98338a2ee5cba3a766a76e3fca79ca09de
garliu <gary.g.liu@nokia-sbell.com>
2019-08-02 14:33:41 +0800

    u

--- src/main/java/HelloHeapServiceImpl.java
+++ src/main/java/HelloHeapServiceImpl.java
@@ -35,7 +35,7 @@ public class HelloHeapServiceImpl implements HelloHeapService {
                 .getCache("cacher", String.class, OffHeapClass.class);
 
 
-        for (int i = 1; i < 10001; i++) {
+        for (int i = 1; i < 10; i++) {
             inHeapCache.put("InHeapKey" + i, new InHeapClass("InHeapKey" + i, "InHeapValue" + i));
             offHeapCache.put("OffHeapKey" + i, new OffHeapClass("OffHeapKey" + i, "OffHeapValue" + i));
         }
